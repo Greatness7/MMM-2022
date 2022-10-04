e0ec88cb719588703b4d9e029fc29bf9bc376fc0
Safebox <safebox36@gmail.com>
2022-10-04 14:40:38 +0100

    Fixed bugs with colour change.

--- Data Files/MWSE/mods/firemoth/vars.lua
+++ Data Files/MWSE/mods/firemoth/vars.lua
@@ -1,14 +1,15 @@
 local var = {
-    fogControl = {
+    skyControl = {
         journalOn = {
             condition = "MS_FargothRing",
             value = 100
         },
         -- journalOff = {condition = "MS_FargothRing", value = 100},
+        cellOn = "Seyda Neen, Census and Excise Office",
         targetObject = "Fargoth",
         triggerDistance = 8192,
         weatherData = {
-            skyColour = tes3vector3.new(0, 255, 0 / 255)
+            airColour = tes3vector3.new(18 / 255, 83 / 255, 89 / 255)
         }
     }
 }
