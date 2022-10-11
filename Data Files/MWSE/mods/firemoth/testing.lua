014362bc7805a6279137a712baef7b566544ca6a
Greatness7 <Greatness7@gmail.com>
2022-10-11 08:00:22 -0400

    Lightning assets + `testing.lua` script (Alt+Z hotkey)

---
+++ Data Files/MWSE/mods/firemoth/testing.lua
@@ -0,0 +1,19 @@
+local lightning = require("firemoth.weather.lightning")
+
+local function onKeyDownZ(e)
+    if not e.isAltDown then return end
+
+    local rayhit = tes3.rayTest {
+        position = tes3.getPlayerEyePosition(),
+        direction = tes3.getPlayerEyeVector(),
+        ignore = tes3.player,
+    }
+
+    if rayhit and rayhit.intersection then
+        local position = rayhit.intersection:copy()
+        lightning.createLightningStrike(position, --[[expode]] true)
+    end
+
+    return false
+end
+event.register(tes3.event.keyDown, onKeyDownZ, { filter = tes3.scanCode.z })
