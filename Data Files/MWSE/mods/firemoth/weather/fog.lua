403e11eb2231596936693c173aa49b5ece9486a9
Greatness7 <Greatness7@gmail.com>
2022-10-21 15:15:07 -0400

    WIP api for for `fog.lua`

--- Data Files/MWSE/mods/firemoth/weather/fog.lua
+++ Data Files/MWSE/mods/firemoth/weather/fog.lua
@@ -1,25 +1,105 @@
 local this = {}
 
+local NUM_FOG_VOLUMES = 2
+
+local fogVolumes = {
+    fogCenters = {
+        0, 0, 0,
+        0, 0, 0,
+    },
+    fogRadi = {
+        0, 0, 0,
+        0, 0, 0,
+    },
+    fogColors = {
+        0, 0, 0,
+        0, 0, 0,
+    },
+    fogDensities = {
+        0,
+        0,
+    },
+}
+
+--- Associates each active fog volume to a specific available index.
+---@type table<string, number>
+local activeFogVolumes = {}
+
 ---@class fogParams
----@field colors number[]
----@field centers number[]
----@field radi number[]
----@field densities number[]
+---@field color tes3vector3
+---@field center tes3vector3
+---@field radius tes3vector3
+---@field density number
 
----comment
----@param t fogParams
----@return mgeShaderHandle
-function this.createFog(t)
-    local shader = mge.shaders.load({ name = "fog_box" })
-    shader:reload()
-    shader.enabled = true
 
-    shader.fogColors = t.colors
-    shader.fogCenters = t.centers
-    shader.fogRadi = t.radi
-    shader.fogDensities = t.densities
-
-    return shader
+local function getNextAvailableIndex()
+    for i = 1, NUM_FOG_VOLUMES do
+        if not activeFogVolumes[i] then
+            return i
+        end
+    end
 end
+
+
+---@param id string
+local function getShaderIndex(id)
+    local index = activeFogVolumes[id]
+    return index or getNextAvailableIndex()
+end
+
+
+---@param i number
+---@param params fogParams
+local function setParamsForIndex(i, params)
+    fogVolumes.fogCenters[i] = params.center.x
+    fogVolumes.fogCenters[i + 1] = params.center.y
+    fogVolumes.fogCenters[i + 2] = params.center.z
+
+    fogVolumes.fogRadi[i] = params.radius.x
+    fogVolumes.fogRadi[i + 1] = params.radius.y
+    fogVolumes.fogRadi[i + 2] = params.radius.z
+
+    fogVolumes.fogColors[i] = params.color.x
+    fogVolumes.fogColors[i + 1] = params.color.y
+    fogVolumes.fogColors[i + 2] = params.color.z
+
+    fogVolumes.fogDensities[i] = params.density
+end
+
+
+local function applyShaderParams()
+    local shader = mge.shaders.load({ name = "fog_box" })
+    if shader then
+        shader.enabled = true
+        shader.fogColors = fogVolumes.fogColors
+        shader.fogCenters = fogVolumes.fogCenters
+        shader.fogRadi = fogVolumes.fogRadi
+        shader.fogDensities = fogVolumes.fogDensities
+    end
+end
+
+
+---@param id string
+---@param params fogParams
+function this.updateFog(id, params)
+    local index = getShaderIndex(id)
+    if index then
+        setParamsForIndex(index, params)
+        applyShaderParams()
+    end
+end
+
+
+---@param id string
+function this.deleteFog(id)
+    local index = getShaderIndex(id)
+    setParamsForIndex(index, {
+        color = tes3vector3.new(),
+        center = tes3vector3.new(),
+        radius = tes3vector3.new(),
+        density = 0,
+    })
+end
+
 
 return this
--- Data Files/MWSE/mods/firemoth/weather/fogController.lua
+++ Data Files/MWSE/mods/firemoth/weather/fogController.lua
@@ -3,23 +3,29 @@ local utils = require("firemoth.utils")
 
 local MAX_DISTANCE = 8192 * 3
 
----@type mgeShaderHandle
-local fogShader = nil
+local fogId = "Firemoth Exterior"
+
+---@type fogParams
+local fogParams = {
+    color = tes3vector3.new(0.07, 0.32, 0.35),
+    center = utils.cells.FIREMOTH_REGION_ORIGIN,
+    radius = tes3vector3.new(MAX_DISTANCE, MAX_DISTANCE, 128),
+    density = 15,
+}
 
 local function update(e)
-    if tes3.player.cell.isInterior or fogShader == nil then
+    if tes3.player.cell.isInterior then
         return
     end
 
     local currDist = utils.cells.getFiremothDistance()
     local prevDist = e.timer.data.prevDist or currDist
 
-    debug.log(utils.math.bellCurve(currDist, 15, 0, MAX_DISTANCE))
-
     if math.min(currDist, prevDist) <= MAX_DISTANCE
         and not math.isclose(currDist, prevDist, 0.001)
     then
-        fogShader.fogDensities[1] = utils.math.bellCurve(currDist, 15, 0, MAX_DISTANCE)
+        fogParams.density = utils.math.bellCurve(currDist, 15, 0, MAX_DISTANCE)
+        fog.updateFog(fogId, fogParams)
     end
 
     e.timer.data.prevDist = currDist
@@ -27,43 +33,21 @@ end
 
 --- @param e cellChangedEventData
 local function cellChangedCallback(e)
-    if (fogShader) then
-        if (e.cell.isInterior) then
-            fogShader.enabled = false
-        else
-            fogShader.enabled = true
-        end
+    if e.cell.isInterior then
+        fog.deleteFog(fogId)
+    else
+        -- ?
     end
 end
+event.register(tes3.event.cellChanged, cellChangedCallback)
 
 local function loadFog()
-    ---@type fogParams
-    local fogParams = {
-        colors = {
-            0.07, 0.32, 0.35,
-            0.0, 0.0, 0.0
-        },
-        centers = {
-            utils.cells.FIREMOTH_REGION_ORIGIN.x, utils.cells.FIREMOTH_REGION_ORIGIN.y, utils.cells.FIREMOTH_REGION_ORIGIN.z,
-            0.0, 0.0, 0.0
-        },
-        radi = {
-            MAX_DISTANCE, MAX_DISTANCE, 128,
-            0.0, 0.0, 0.0
-        },
-        densities = {
-            15,
-            0
-        }
-    }
-    fogShader = fog.createFog(fogParams)
+    fog.updateFog(fogId, fogParams)
 
-    if (tes3.getPlayerCell().isInterior) then
-        fogShader.enabled = false
+    if tes3.player.cell.isInterior then
+        fog.deleteFog(fogId)
     end
 
     timer.start({ iterations = -1, duration = 1 / 10, callback = update, data = {} })
 end
-
-event.register(tes3.event.cellChanged, cellChangedCallback)
 event.register(tes3.event.loaded, loadFog)
