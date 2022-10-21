0646928200baabe879750d3530b510400a98bc8b
Greatness7 <Greatness7@gmail.com>
2022-10-21 15:19:47 -0400

    Fix up some naming.

--- Data Files/MWSE/mods/firemoth/weather/fog.lua
+++ Data Files/MWSE/mods/firemoth/weather/fog.lua
@@ -42,7 +42,7 @@ end
 
 
 ---@param id string
-local function getShaderIndex(id)
+local function getFogVolumeIndex(id)
     local index = activeFogVolumes[id]
     return index or getNextAvailableIndex()
 end
@@ -82,7 +82,7 @@ end
 ---@param id string
 ---@param params fogParams
 function this.updateFog(id, params)
-    local index = getShaderIndex(id)
+    local index = getFogVolumeIndex(id)
     if index then
         setParamsForIndex(index, params)
         applyShaderParams()
@@ -92,7 +92,7 @@ end
 
 ---@param id string
 function this.deleteFog(id)
-    local index = getShaderIndex(id)
+    local index = getFogVolumeIndex(id)
     setParamsForIndex(index, {
         color = tes3vector3.new(),
         center = tes3vector3.new(),
