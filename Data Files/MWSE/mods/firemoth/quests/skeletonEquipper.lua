b41120874c93f4ca1477e1a9625ecccb5be8b90a
Greatness7 <Greatness7@gmail.com>
2022-10-31 04:33:37 -0400

    Simplify skeleton equipper.

--- Data Files/MWSE/mods/firemoth/quests/skeletonEquipper.lua
+++ Data Files/MWSE/mods/firemoth/quests/skeletonEquipper.lua
@@ -1,63 +1,42 @@
+local targetMeshes = table.invert({
+    "meshes\\fm\\r\\xskeleton_rising_1.nif",
+    "meshes\\fm\\r\\xskeleton_rising_2.nif",
+})
+
+local targetCreatures = table.invert({
+    assert(tes3.getObject("fm_skeleton_1")),
+    assert(tes3.getObject("fm_skeleton_2")),
+})
+
 ---@type tes3armor[]
-local armorObjects = {
+local targetArmors = {
     assert(tes3.getObject("imperial boots")),
+    assert(tes3.getObject("imperial cuirass_armor")),
     assert(tes3.getObject("imperial helmet armor")),
-    assert(tes3.getObject("imperial left gauntlet")),
-    assert(tes3.getObject("imperial left pauldron")),
     assert(tes3.getObject("imperial right gauntlet")),
     assert(tes3.getObject("imperial right pauldron")),
-    assert(tes3.getObject("imperial cuirass_armor")),
     assert(tes3.getObject("imperial skirt_clothing")),
 }
 
-local nodes = {}
-do
-    local mesh = tes3.loadMesh("base_anim.nif")
-    for _, name in pairs({
-        "Chest",
-        "Groin",
-        "Head",
-        "Left Ankle",
-        "Left Clavicle",
-        "Left Foot",
-        "Left Forearm",
-        "Left Hand",
-        "Left Knee",
-        "Left Upper Arm",
-        "Left Upper Leg",
-        "Left Wrist",
-        "Neck",
-        "Right Ankle",
-        "Right Clavicle",
-        "Right Foot",
-        "Right Forearm",
-        "Right Hand",
-        "Right Knee",
-        "Right Upper Arm",
-        "Right Upper Leg",
-        "Right Wrist",
-        "Shield",
-        "Weapon",
-    }) do
-        local node = assert(mesh:getObjectByName(name))
-        nodes[node:clone()] = node.parent.name
-    end
-end
-
-local partConfig = {
-    [1] = { name = "Head", offset = tes3vector3.new(0, 2.8, 2.0) },
+local partConfigs = {
+    [1] = { name = "Head", translation = tes3vector3.new(0, 2.8, 2.0) },
     [4] = { name = "Chest" },
     [8] = { name = "Right Upper Arm" },
     [9] = { name = "Right Foot" },
     [10] = { name = "Right Ankle" },
     [13] = { name = "Right Clavicle" },
 }
+
+local regular = { "Head", "Right Upper Arm", "Right Foot", "Right Ankle", "Right Clavicle" }
+local skinned = { "Groin", "Chest" }
+
+
 local function attachRegular(sceneNode, partNode, partIndex)
-    local config = partConfig[partIndex]
+    local config = partConfigs[partIndex]
     if config then
         sceneNode:getObjectByName(config.name):attachChild(partNode)
-        if config.offset then
-            partNode.translation = partNode.translation + config.offset
+        if config.translation then
+            partNode.translation = partNode.translation + config.translation
         end
     end
 end
@@ -84,12 +63,7 @@ local function isSkinned(root)
 end
 
 local function patchMesh(sceneNode)
-    for node, parentName in pairs(nodes) do
-        local parentNode = assert(sceneNode:getObjectByName(parentName))
-        local attachNode = node:clone()
-        parentNode:attachChild(attachNode)
-    end
-    for _, armor in pairs(armorObjects) do
+    for _, armor in pairs(targetArmors) do
         for _, p in pairs(armor.parts) do
             if p.male then
                 local root = tes3.loadMesh(p.male and p.male.mesh)
@@ -105,35 +79,27 @@ local function patchMesh(sceneNode)
     end
 end
 
-
-local targetMeshes = table.invert({
-    "meshes\\fm\\r\\skeleton_rising_1.nif",
-    "meshes\\fm\\r\\xskeleton_rising_1.nif",
-    "meshes\\fm\\r\\skeleton_rising_2.nif",
-    "meshes\\fm\\r\\xskeleton_rising_2.nif",
-})
 event.register(tes3.event.meshLoaded, function(e)
+    ---@cast e meshLoadedEventData
     if targetMeshes[e.path:lower()] then
         patchMesh(e.node)
     end
 end)
 
-
-local targetObjects = table.invert({
-    assert(tes3.getObject("fm_skeleton_1")),
-    assert(tes3.getObject("fm_skeleton_2")),
-})
 event.register(tes3.event.referenceSceneNodeCreated, function(e)
-    if targetObjects[e.reference.baseObject] then
+    ---@cast e referenceSceneNodeCreatedEventData
+    if targetCreatures[e.reference.baseObject] then
         local bip01 = e.reference.sceneNode:getObjectByName("Bip01")
-        for _, name in pairs({ "Head", "Right Upper Arm", "Right Foot", "Right Ankle", "Right Clavicle" }) do
+        -- Show random regular equipment.
+        for _, name in pairs(regular) do
             if math.random() > 0.5 then
                 local node = bip01:getObjectByName(name)
                 node.appCulled = true
                 node:update()
             end
         end
-        for _, name in pairs({ "Groin", "Chest" }) do
+        -- Show random skinned equipment.
+        for _, name in pairs(skinned) do
             if math.random() > 0.5 then
                 for _, child in pairs(bip01.children) do
                     if child.name:gsub("Tri ", ""):startswith(name) then
