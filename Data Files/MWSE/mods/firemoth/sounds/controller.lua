b2df2edb91d9ffdd7a3c73ed6bfdae9b79b539ad
Greatness7 <Greatness7@gmail.com>
2022-10-31 17:54:42 -0400

    Use non-repeating random for interior sounds.

--- Data Files/MWSE/mods/firemoth/sounds/controller.lua
+++ Data Files/MWSE/mods/firemoth/sounds/controller.lua
@@ -1,38 +1,41 @@
 local utils = require("firemoth.utils")
-local isFiremothCell = utils.cells.isFiremothCell
 
-local durations = { 5, 8, 10, 12, 14, 15, 18, 20, 23, 25, 28, 30 }
+---@type mwseTimer|nil
 local interiorTimer
+
+local sounds = {}
+for i = 1, 14 do
+    table.insert(sounds, tes3.getSound("tew_fm_int" .. i))
+end
+local durations = { 5, 8, 10, 12, 14, 15, 18, 20, 23, 25, 28, 30 }
+
+local randomSound = utils.math.nonRepeatTableRNG(sounds)
+local randomDuration = utils.math.nonRepeatTableRNG(durations)
 
 local function playInteriorSound()
     tes3.playSound({
-        sound = "tew_fm_int" .. math.random(1, 14),
+        sound = randomSound(),
         reference = tes3.player,
-        volume = 1.0 * math.random(4, 10) / 10,
+        volume = math.random() * 0.6 + 0.4,
         mixChannel = tes3.soundMix.master,
     })
 end
 
 local function resolveCell()
-    local cell = tes3.getPlayerCell()
+    if (interiorTimer) and not (interiorTimer.state == timer.expired) then
+        interiorTimer:cancel()
+        interiorTimer = nil
+    end
 
-    if cell.isInterior and isFiremothCell(cell) then
-        local dur = table.choice(durations)
-        interiorTimer = timer.start {
-            type = timer.simulate,
-            duration = dur,
+    local cell = tes3.player.cell
+    if cell.isInterior and utils.cells.isFiremothInterior(cell) then
+        interiorTimer = timer.start({
+            duration = randomDuration(),
             callback = function()
                 playInteriorSound()
                 resolveCell()
             end,
-        }
-    else
-        if (interiorTimer) and not (interiorTimer.state == timer.expired) then
-            interiorTimer:pause()
-            interiorTimer:cancel()
-            interiorTimer = nil
-        end
+        })
     end
 end
-
 event.register(tes3.event.cellChanged, resolveCell)
